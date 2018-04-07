85fd0484a240b523ef94d2aae736bb1029741f4e
Skyost <me@skyost.eu>
2018-04-07 16:24:20 +0200

    Some fixes.

--- api/settings/Others.php
+++ api/settings/Others.php
@@ -3,9 +3,11 @@
 $settings['APP_NAME'] = 'AdSky';
 $settings['APP_WEBSITE'] = 'https://dev.bukkit.org/projects/adsky';
 $settings['APP_CURRENCY'] = 'USD';
-$settings['APP_DEBUG'] = true; // TODO
+$settings['APP_DEBUG'] = true; // TODO Remove this on release
 
 $settings['DB_PREFIX'] = 'adsky_';
-$settings['DB_THROTTLING'] = !$settings['APP_DEBUG']; // TODO
+$settings['DB_THROTTLING'] = !$settings['APP_DEBUG'];
 
 $settings['PAGINATOR_MAX'] = 10;
+
+$settings['EMAIL_SENDER'] = 'adsky@skyost.eu';
--- api/user/delete.php
+++ api/user/delete.php
@@ -10,6 +10,5 @@ $username = empty($_POST['username']) ? $auth -> getEmail() : $_POST['username']
 if($username != $auth -> getUsername() && !$auth -> hasRole(\Delight\Auth\Role::ADMIN)) {
     (new Response($lang['API_ERROR_NOT_ADMIN'])) -> returnResponse();
 }
-// TODO Vérifier les vulnérabilités : si un utilisateur change son email dans le cookie par exemple.
 
 ((new User(null, null, $username)) -> delete($pdo, $auth)) -> returnResponse();
